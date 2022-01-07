ed6ee7bfd8c0a0d096a169993bb1d0cd96346766
Rahul kumar M S <rahulmanakyalath@gmail.com>
2022-01-07 14:08:24 +0530

    initial commit

---
+++ src/main/java/com/example/RkMail/RkMailApplication.java
@@ -0,0 +1,13 @@
+package com.example.RkMail;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RkMailApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RkMailApplication.class, args);
+	}
+
+}
